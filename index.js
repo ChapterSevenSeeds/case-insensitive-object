5d1aeaa2312df2e76576160cd6ad795a8aa3b718
Tyson <tyson.jones@customerdynamics.com>
2022-03-30 12:25:38 -0600

    More

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const handler = {
     set: function (target, prop, value) {
         const lowercase = prop.toLowerCase();
         target[lowercase] = [prop, value]
+    },
+    ownKeys: function(target) {
+        return Object.values(target).map(value => value[0]);
     }
 };
 
 const proxy = new Proxy({}, handler);
 
-proxy["PROP"] = 3;
-console.log(proxy["prop"]); // "original value"
+proxy["PROP"] = 1;
+proxy["AsDf"] = 2;
+console.log(proxy["prop"]);
+console.log(proxy["asdf"]);
+
+const asdfasdf = Object.keys(proxy)
