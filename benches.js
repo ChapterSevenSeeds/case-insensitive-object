2c8d9ca9d730a0f6703ff60677244086805d3ac5
Tyson Jones <ballisticmissle@gmail.com>
2022-03-31 15:35:23 -0600

    Added some benchmarks

---
+++ benches.js
@@ -0,0 +1,32 @@
+const mine = require('./index');
+const theirs = require('case-insensitive-object').default;
+const prettyMilliseconds = require('pretty-ms');
+const { performance } = require('perf_hooks');
+const { v4: uuidv4 } = require('uuid');
+
+const TEST_COUNT = 50000;
+
+const keys = new Set();
+for (let i = 0; i < TEST_COUNT; ++i) {
+    keys.add(uuidv4());
+}
+
+let start = performance.now();
+let obj = theirs({});
+for (const key of keys) {
+    obj[key] = 3;
+}
+for (const key of keys) {
+    delete obj[key]
+}
+console.log("Theirs: ", prettyMilliseconds(performance.now() - start));
+
+start = performance.now();
+obj = new mine({});
+for (const key of keys) {
+    obj[key] = 3;
+}
+for (const key of keys) {
+    delete obj[key]
+}
+console.log("Mine: ", prettyMilliseconds(performance.now() - start));
